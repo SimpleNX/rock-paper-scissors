3dd56a88f641a13d0ec73b30723d6846a9fc386b
SimpleNX <simplepanigrahi17@gmail.com>
2024-07-12 11:16:18 +0530

    Edit game.js
    Edit game.js to add playGame function to play the
    game for 5 rounds each refresh.
    Change random number generator so that the better
    random numbers are generated, with the previous
    version the choices were not random.

--- game.js
+++ game.js
@@ -1,11 +1,8 @@
 
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice(){
     const arr = ["rock", "paper", "scissors"];
-    const idx = Math.floor(Math.random()*2);
-    console.log(idx);
+    const idx = Math.floor(Math.random()*100)%3;
     return arr[idx];
 }
 
@@ -14,46 +11,60 @@ function getHumanChoice(){
     return choice.toLowerCase();
 }
 
-function whoWon(humanChoice, computerChoice){
-    let result = -1;
-    if(humanChoice === computerChoice)
-        return -1;
-    else if(humanChoice === "rock" && computerChoice === "paper" || 
-        humanChoice === "paper" && computerChoice === "scissors" ||
-        humanChoice === "scissors" && computerChoice === "rock"){
-        result = 1;
-        computerScore++;
+function playGame(){
+    let whoWon = function (humanChoice, computerChoice){
+        let result = -1;
+        if(humanChoice === computerChoice)
+            return -1;
+        else if(humanChoice === "rock" && computerChoice === "paper" || 
+            humanChoice === "paper" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice === "rock"){
+            result = 1;
+            computerScore++;
+        }
+        else if(computerChoice === "rock" && humanChoice === "paper" || 
+            computerChoice === "paper" && humanChoice === "scissors" ||
+            computerChoice === "scissors" && humanChoice === "rock"){
+            result = 0;
+            humanScore++;
+        }
+        return result;
+    };
+    let playRound = function (humanChoice, computerChoice){
+        const choices = [humanChoice, computerChoice];
+        console.log(choices);
+        let result = whoWon(humanChoice, computerChoice);
+        if(result === -1){
+            console.log("It's a tie!");
+            return;
+        }
+        let message = "";
+        let other = 0;
+        if(result == 0){
+            message += "You won!";
+            other =1;
+        }
+        else
+            message += "You lose!";
+        console.log(message + " " + `${choices[result]} beats ${choices[other]}`); 
+    };
+    let humanScore = 0;
+    let computerScore = 0;
+    for(i = 0; i<5; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
     }
-    else if(computerChoice === "rock" && humanChoice === "paper" || 
-        computerChoice === "paper" && humanChoice === "scissors" ||
-        computerChoice === "scissors" && humanChoice === "rock"){
-        result = 0;
-        humanScore++;
-    }
-    return result;
-}
-
-function playRound(humanChoice, computerChoice){
-    const choices = [humanChoice, computerChoice];
-    console.log(choices);
-    let result = whoWon(humanChoice, computerChoice);
-    if(result === -1){
-        console.log("It's a tie!");
-        return;
-    }
-    let message = "";
-    let other = 0;
-    if(result == 0){
-        message.concat("You won!");
-        other =1;
-    }
+    let finalMessage = "";
+    if(humanScore > computerScore)
+        finalMessage += "You win!";
+    else if(humanScore === computerScore)
+        finalMessage += "It's a tie";
     else
-        message.concat("You lose!");
-    console.log(message + " " + `${choices[result]} beats ${choices[other]}`); 
+        finalMessage += "You lose!";
+    console.log(finalMessage + " " + `Score : ${humanScore} : ${computerScore}`);
 }
 
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
+playGame();
 
-playRound(humanChoice, computerChoice);
 
